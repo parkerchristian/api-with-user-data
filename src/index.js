2adece87de5e6724f44ffecbf7d6b8062af371a4
Parker Helland <hellandp@gmail.com>
2019-03-14 13:01:30 -0700

    css album cards

--- src/index.js
+++ src/index.js
@@ -28,25 +28,25 @@ window.addEventListener('hashchange', () => {
     const searchOptions = readFromQuery(existingQuery);
     baseGetUrl = makeURL(searchOptions);
     console.log('!!!', baseGetUrl);
+    // const baseGetUrl = 'https://api.spotify.com/v1/search?q=cher&type=album&market=US';
+    const getUrl = corsAnywhereUrl + baseGetUrl;
+    
+    fetch(postUrl, postOptions)
+        .then(response => response.json())
+        .then(token => {
+            const getOptions = {
+                headers: {
+                    Authorization: `${token.token_type} ${token.access_token}`
+                }
+            };
+            return getOptions;
+        })
+        .then(getOptions => {
+            fetch(getUrl, getOptions)
+                .then(response => response.json())
+                .then(body => {
+                    loadAlbums(body.albums.items);
+                });
+        });
 });
 
-// const baseGetUrl = 'https://api.spotify.com/v1/search?q=cher&type=album&market=US';
-const getUrl = corsAnywhereUrl + baseGetUrl;
-
-// fetch(postUrl, postOptions)
-//     .then(response => response.json())
-//     .then(token => {
-//         const getOptions = {
-//             headers: {
-//                 Authorization: `${token.token_type} ${token.access_token}`
-//             }
-//         };
-//         return getOptions;
-//     })
-//     .then(getOptions => {
-//         fetch(getUrl, getOptions)
-//             .then(response => response.json())
-//             .then(body => {
-//                 loadAlbums(body.albums.items);
-//             });
-//     });
