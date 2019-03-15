7a32d56fa14bf1db320cb5c1d3914083a4b87f52
Parker Helland <hellandp@gmail.com>
2019-03-15 10:54:08 -0700

    made paging tests and offset calc function, all passing

--- test/paging.test/paging-component.test.js
+++ test/paging.test/paging-component.test.js
@@ -2,11 +2,47 @@ const test = QUnit.test;
 
 QUnit.module('PAGING COMPONENT TESTS');
 
-test('Page', assert => {
+function currentPageToOffset(options) {
+
+    const offset = ((options.currentPage - 1) * options.limit);
+
+    const offsetObject = {
+        offset: offset,
+        limit: options.limit
+    };
+    return offsetObject;
+}
+
+test('Page 3 of 5 with limit 20', assert => {
     //Arrange
-
+    const options = {
+        limit: 20,
+        currentPage: 3,
+        totalPages: 5
+    };
+    const expected = {
+        offset: 40,
+        limit: 20
+    };
     //Act
-
+    const result = currentPageToOffset(options);
     //Assert
-    assert.deepEqual();
+    assert.deepEqual(result, expected);
+});
+
+test('Page 5 of 5 with limit 13', assert => {
+    //Arrange
+    const options = {
+        limit: 13,
+        currentPage: 5,
+        totalPages: 5
+    };
+    const expected = {
+        offset: 52,
+        limit: 13
+    };
+    //Act
+    const result = currentPageToOffset(options);
+    //Assert
+    assert.deepEqual(result, expected);
 });
