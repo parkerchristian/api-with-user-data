8569153aabefacdc51da0a8469561b836a58f21b
Parker Helland <hellandp@gmail.com>
2019-03-15 11:08:09 -0700

    updated hash-query to include limit and paging passing test

--- src/hash-query/query-components.js
+++ src/hash-query/query-components.js
@@ -1,13 +1,17 @@
 export function readFromQuery(existingQuery) {
     const searchParams = new URLSearchParams(existingQuery);
     const queryObject = {
-        q: searchParams.get('q')
+        q: searchParams.get('q'),
+        limit: parseInt(searchParams.get('limit')),
+        offset: parseInt(searchParams.get('offset'))
     };
     return queryObject;
 }
 
-export function writeToQuery(existingQuery, searchTerm) {
+export function writeToQuery(existingQuery, options) {
     const searchParams = new URLSearchParams(existingQuery);
-    searchParams.set('q', searchTerm);
+    searchParams.set('q', options.searchTerm);
+    searchParams.set('limit', options.limit);
+    searchParams.set('offset', options.offset);
     return searchParams.toString();
 }
--- test/query.tests/query.test.js
+++ test/query.tests/query.test.js
@@ -7,10 +7,14 @@ QUnit.module('QUERY TESTS');
 test('write search to query test', assert => {
     // arrange
     const existingQuery = '';
-    const searchTerm = 'cher';
-    const expected = 'q=cher';
+    const options = {
+        searchTerm: 'cher',
+        limit: 20,
+        offset: 0
+    };
+    const expected = 'q=cher&limit=20&offset=0';
     // act
-    const result = writeToQuery(existingQuery, searchTerm);
+    const result = writeToQuery(existingQuery, options);
     // assert
     assert.equal(result, expected);
 });
@@ -18,19 +22,25 @@ test('write search to query test', assert => {
 test('write a search to an existing query', assert => {
     // arrange
     const existingQuery = 'q=cher';
-    const searchTerm = 'Motley Crue';
-    const expected = 'q=Motley+Crue';
+    const options = {
+        searchTerm: 'Motley Crue',
+        limit: 20,
+        offset: 0
+    };
+    const expected = 'q=Motley+Crue&limit=20&offset=0';
     // act
-    const result = writeToQuery(existingQuery, searchTerm);
+    const result = writeToQuery(existingQuery, options);
     // assert
     assert.equal(result, expected);
 });
 
 test('read query to object', assert => {
     //Arrange
-    const existingQuery = 'q=Motley+Crue';
+    const existingQuery = 'q=Motley+Crue&limit=20&offset=40';
     const expected = {
         q: 'Motley Crue',
+        limit: 20,
+        offset: 40
     };
     //Act
     const result = readFromQuery(existingQuery);
