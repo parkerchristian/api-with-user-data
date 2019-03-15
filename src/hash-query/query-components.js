df677053ea01bd6a7647d2dc2c1d7ee465285621
Parker Helland <hellandp@gmail.com>
2019-03-15 11:55:21 -0700

    updated query functions to take paging with passing tests

--- src/hash-query/query-components.js
+++ src/hash-query/query-components.js
@@ -1,16 +1,16 @@
 export function readFromQuery(existingQuery) {
     const searchParams = new URLSearchParams(existingQuery);
     const queryObject = {
-        q: searchParams.get('q'),
-        limit: parseInt(searchParams.get('limit')),
-        offset: parseInt(searchParams.get('offset'))
+        q: searchParams.get('q') ? searchParams.get('q') : false,
+        limit: searchParams.get('limit') ? parseInt(searchParams.get('limit')) : 20,
+        offset: searchParams.get('offset') ? parseInt(searchParams.get('offset')) : 0
     };
     return queryObject;
 }
 
 export function writeToQuery(existingQuery, options) {
     const searchParams = new URLSearchParams(existingQuery);
-    searchParams.set('q', options.searchTerm);
+    options.searchTerm ? searchParams.set('q', options.searchTerm) : false;
     searchParams.set('limit', options.limit);
     searchParams.set('offset', options.offset);
     return searchParams.toString();
--- test/query.tests/query.test.js
+++ test/query.tests/query.test.js
@@ -1,4 +1,5 @@
 import { readFromQuery, writeToQuery } from '../../src/hash-query/query-components.js';
+import { currentPageToOffset } from '../../src/paging/paging-functions.js';
 
 const test = QUnit.test;
 
@@ -46,4 +47,20 @@ test('read query to object', assert => {
     const result = readFromQuery(existingQuery);
     //Assert
     assert.deepEqual(result, expected);
+});
+
+test('on page change update query', assert => {
+    // arrange
+    const options = {
+        currentPage: 3,
+        totalPages: 10,
+        limit: 20
+    };
+    const expected = 'q=Motley+Crue&limit=20&offset=40';
+    const existingQuery = 'q=Motley+Crue&limit=20&offset=20';
+    // act
+    const offsetOptions = currentPageToOffset(options);
+    const result = writeToQuery(existingQuery, offsetOptions);
+    // assert
+    assert.equal(result, expected);
 });
